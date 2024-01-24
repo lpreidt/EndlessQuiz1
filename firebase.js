e8471f3be14c38c6bdabba5fc9cc975dd1ba10ea
lpreidt <lpreidt@gmail.com>
2024-01-24 16:36:30 +0100

    fxed

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBR5Gs1nazB-1tH9r6-FU855T1FrD_f6eY",
-  authDomain: "endlessquiz-e5c0b.firebaseapp.com",
-  projectId: "endlessquiz-e5c0b",
-  storageBucket: "endlessquiz-e5c0b.appspot.com",
-  messagingSenderId: "672953767220",
-  appId: "1:672953767220:web:14e6695afb417b15410161",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
